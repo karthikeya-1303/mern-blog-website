995d3a913e4b6ce5677ea2c7385147807539413d
Pendyala Koushik <157451745+karthikeya-1303@users.noreply.github.com>
2024-07-09 15:01:49 +0530

    Update Footer.js

--- Frontend/src/components/GeneralScreens/Footer.js
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'C:/Users/karth/OneDrive/Desktop/Mern-Blog-main/Frontend/src/Css/Footer.css'; 
+import '../../Css/Footer.css'; 
 const Footer = () => {
   return (
     <footer className="footer">
